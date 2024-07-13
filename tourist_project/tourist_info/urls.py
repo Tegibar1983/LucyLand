8da5056bc9c0ef8474439e7ec4c9b6cc57e3e5b9
Tegibar1983 <tbtf1991@gmail.com>
2024-07-13 06:39:45 -0700

    forms.py

--- tourist_project/tourist_info/urls.py
+++ tourist_project/tourist_info/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'), #127.0.0.1:8000/home/
+    path('create_region/', views.create_region, name='create_region'), #127.0.0.1:8000/create_region
 ]
--- tourist_project/tourist_info/views.py
+++ tourist_project/tourist_info/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TouristInfo
+from .forms import TouristInfoForm
 
 # Create your views here.
 
@@ -9,4 +10,16 @@ def home(request):
     context={'all_regions' : all_regions}
     return render(request, 'home.html', context=context)
 
-
+def create_region(request):
+    if request.method=="POST":
+        form=TouristInfoForm(request.POST, request.FILES)
+        context={'form':form}
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'create_region.html', context=context)
+    else:
+        form=TouristInfoForm()
+        context={'form':form}
+        return render(request, 'create_region.html', context=context)
