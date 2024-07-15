1203bf47f6aa59dca5966e504cddc8c0c5f2a729
Tegibar1983 <tbtf1991@gmail.com>
2024-07-15 13:24:08 -0700

    updated Update and Delete functionalities

--- tourist_project/tourist_info/views.py
+++ tourist_project/tourist_info/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import TouristInfo
 from .forms import TouristInfoForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,3 +31,31 @@ def read_more(request, pk):
     region=TouristInfo.objects.get(id=pk)
     context={'region':region}
     return render(request, 'read_more.html', context=context)
+
+
+def update_region(request, pk):
+    region=TouristInfo.objects.get(id=pk)
+    form=TouristInfoForm(instance=region)
+    if request.method=="POST":
+        form=TouristInfoForm(request.POST, request.FILES, instance=region)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            messages.success(request, 'Sorry Region information is not updated due to invalid form submission')
+            return render(request, 'update_region.html', context=context)
+    else:
+        context={'form':form}
+        return render(request, 'update_region.html', context=context)
+    
+
+
+def delete_region(request, pk):
+    region=TouristInfo.objects.get(id=pk)
+    if request.method=="POST":
+        region.delete()
+        messages.success(request, 'Region information is deleted successfully')
+        return redirect('home')
+    else:
+        context={'region':region}
+        return render(request, 'delete_region.html', context=context)
