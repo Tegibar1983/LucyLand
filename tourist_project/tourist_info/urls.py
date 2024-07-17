e5d9e3f58d989aba070fa0c073bba628fe9d7fd4
Tegibar1983 <tbtf1991@gmail.com>
2024-07-16 17:36:57 -0700

    Add extra routing for home page

--- tourist_project/tourist_info/urls.py
+++ tourist_project/tourist_info/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'), #127.0.0.1:8000/home/
     path('home/', views.home, name='home'), #127.0.0.1:8000/home/
     path('create_region/', views.create_region, name='create_region'), #127.0.0.1:8000/create_region
     path('read_more/<int:pk>', views.read_more, name='read_more'), #127.0.0.1:8000/read_more
