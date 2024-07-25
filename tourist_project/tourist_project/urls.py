000c2e19233f90840668ba80fda5b844a0f6b62f
Mihretu Tesfaye Haile <MIHERET.TESFAYE@ethiotelecom.et>
2024-07-25 16:09:44 +0300

    Fix merge conflicts for migration files

--- tourist_project/tourist_project/urls.py
+++ tourist_project/tourist_project/urls.py
@@ -14,26 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-=======
+from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('tourist_Info.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), #127.0.0.1/accounts
+    path('', include('tourist_info.urls')),  # Ensure this matches your app name
+    path('accounts/', include('django.contrib.auth.urls')),  # 127.0.0.1/accounts
     path('admin/', admin.site.urls),
 ]
-<<<<<<< HEAD
->>>>>>> 8da5056bc9c0ef8474439e7ec4c9b6cc57e3e5b9
-=======
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> e5d9e3f58d989aba070fa0c073bba628fe9d7fd4
+
+# Optionally, add static files handling in development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
